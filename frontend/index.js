845ca3c20aba37457c483e096e591c31ac52e89e
Spoorthi <spoorthirama34@gmail.com>
2026-08-14 11:10:35 +0530

    random

--- frontend/index.js
+++ frontend/index.js
@@ -14,16 +14,27 @@ const users = [
 
 var curIndex = 0;
 
-
 function toggle(){
  if(curIndex == 0)
    curIndex = 1;
  else
    curIndex = 0;
 
-
  document.getElementById("card-image").src = users[curIndex].image
  document.getElementById("card-name").innerText =users[curIndex].name
  document.getElementById("card-gender").innerText =users[curIndex].gender
 }
 
+function random(){
+  fetch('https://randomuser.me/api/')
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    var  detail = data.results[0];
+    document.getElementById("card-image").src = detail.picture.large;
+    document.getElementById("card-gender").src = detail.gender;
+    var fullname = detail.name.title+"  "+detail.name.first+"  "+detail.name.last
+    document.getElementById("card-name").innerText = fullname;
+  })
+}
